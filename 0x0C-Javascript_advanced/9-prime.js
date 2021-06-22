010cbefe45a206e0bd1bcbb941acf7c0da1d7f98
Leidy J <44031927+Leidysalda@users.noreply.github.com>
2021-06-22 14:14:43 -0500

    [Add]file 9-prime.js

---
+++ 0x0C-Javascript_advanced/9-prime.js
@@ -0,0 +1,26 @@
+function countPrimeNumbers () {
+    function isPrime (num) {
+        for (var i = 2; i < num; i++) {
+            if (num % i === 0) {
+                return false;
+            }
+        }
+    return true;
+}
+
+function display (n) {
+    let arr = [2];
+    for (var i = 3; i < n; i += 2) {
+        if (isPrime(i)) {
+            arr.push(i);
+        }
+    }
+    console.log(arr);
+}
+display(100);
+}
+
+const timeBefore = performance.now();
+countPrimeNumbers();
+const timeAfter = performance.now();
+console.log(`Execution time of printing countPrimeNumbers was ${timeAfter -timeBefore} milliseconds.`);
